fd3297f8985a71e144fc3765c7ebc6c74b4f1232
Andreas Reitz <reitz@cnc-technik.de>
2020-06-12 13:43:58 +0200

    Name hinzugefügt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,21 +33,23 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(271, 151);
+            this.button1.Location = new System.Drawing.Point(361, 186);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(459, 176);
+            this.button1.Size = new System.Drawing.Size(612, 217);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Thread";
             this.ResumeLayout(false);
 
         }
